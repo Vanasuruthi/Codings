cab889116d502c1ef36650a88513b89cadc4a70a
Vanasuruthi Murugesan <vanasuruthimurugesan@gmail.com>
2017-04-21 21:33:13 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class Factorial
+{
+  public static void main (String args[]) throws IOException
+  {
+    int fact=1;
+    DataInputStream d =new DataInputStream(System.in);
+    System.out.println("Enter a Number:\n");
+    int n=Integer.parseInt(d.readLine());
+    for(int i=n; i>n; i++)
+    {
+        fact=fact*i;
+    }
+    System.out.println("Factorial of "+n+" is: " +fact+ );
+  }
+}
