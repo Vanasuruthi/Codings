f035038b2b78a9ae620abfd8c454f2f7a00374c5
Vanasuruthi Murugesan <vanasuruthimurugesan@gmail.com>
2017-04-22 14:29:59 +0530

    Update ReverseInteger.java

--- ReverseInteger.java
+++ ReverseInteger.java
@@ -1,17 +1,18 @@
-import java.io.*
+import java.io.*;
 public class ReverseInteger
 {
-  public static void main(String args[])
+  public static void main(String args[])throws IOException
   {
       int revInt=0,lastDigit;
       DataInputStream d= new DataInputStream(System.in);
       int n= Integer.parseInt(d.readLine());
+      int num=n;
       while(n>0)
       {
           lastDigit=n%10;
           revInt=(revInt*10)+lastDigit;
           n=n/10;
       }
-      System.out.println("Reverse of Given Integer "+n+" is: "+revInt);
+      System.out.println("Reverse of Given Integer "+num+" is: "+revInt);
   }
 }
