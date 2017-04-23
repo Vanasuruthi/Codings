42b9e94942cb90912583e9b79e5fbebbc38ad588
Vanasuruthi Murugesan <vanasuruthimurugesan@gmail.com>
2017-04-23 20:09:15 +0530

    Create RemoveDuplicate.java

---
+++ RemoveDuplicate.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class RemoveDuplicate
+{
+    public static void main(String args[])throws IOException
+    {
+        DataInputStream d=new DataInputStream(System.in);
+        String str=d.readLine();
+        char[] c=new char[];
+        c=str.toCharArray();
+        for(int i=0; i<c.length; i++)
+        {
+            
+        }
+    }
+}
